3a8505d05d0448f06866237e7bd872535aa653ef
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-04-07 06:01:09 +0200

    added Clock object

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock() # create a Clock object
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while screen:
+    
+    while True: # infinite while loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000 # convert from milliseconds to seconds
 
 
 
